e5be07623429b47ceece04a5e32071b82e5c7da3
AbuTumming <48900838+AbuTumming@users.noreply.github.com>
2019-03-25 14:26:15 +0800

    Update unfollall.js

--- unfollall.js
+++ unfollall.js
@@ -110,10 +110,11 @@ console.log(chalk`
   [?] {bold.green UNFALL | Unfollow All Following IG!}
 
   ——————————————————  [THANKS TO]  ————————————————————
-  [✓] CODE BY CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
+   [✓] CODE BY CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
   [✓] FIXING & TESTING BY SYNTAX (@officialputu_id)
   [✓] CCOCOT.CO | BC0DE.NET | NAONLAH.NET | WingkoColi
   [✓] SGB TEAM REBORN | Zerobyte.id | ccocot@bc0de.net 
+  [✓] ./LaGongrong | TerbujurKaku | @AnakLorongMks
   —————————————————————————————————————————————————————
   What's new?
   1. Input Target/delay Manual (ITTYW)
