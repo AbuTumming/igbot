3897d4fcd99923ec41ffbaac9d161c15fb18ac31
AbuTumming <48900838+AbuTumming@users.noreply.github.com>
2019-03-25 14:32:16 +0800

    Update fft.js

--- fft.js
+++ fft.js
@@ -211,6 +211,7 @@ console.log(chalk`
   [✓] FIXING & TESTING BY SYNTAX (@officialputu_id)
   [✓] CCOCOT.CO | BC0DE.NET | NAONLAH.NET | WingkoColi
   [✓] SGB TEAM REBORN | Zerobyte.id | ccocot@bc0de.net 
+  [✓] ./LaGongrong | TerbujurKaku | @AnakLorongMks 
   —————————————————————————————————————————————————————
   What's new?
   1. Input Target/delay Manual (ITTYW)
